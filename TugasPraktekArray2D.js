598195ed4c7b6d5fadd76e042a526c2c8f02743d
Aryosatyo Bhanurahman <bhanurahman919@gmail.com>
2023-03-07 21:18:40 +0700

    inisiasi project

---
+++ TugasPraktekArray2D.js
@@ -0,0 +1,14 @@
+let semua = [
+    ['ayam', 'botol', 'hape','kunci','dompet'],
+    ['spiderman', 'thor', 'magerman','hulk','loki'],
+    ['makan', 'minum', 'tidur','tidur lagi','tidur terus'],
+];
+console.log(semua[0][0]);
+console.log(semua[0][2]);
+console.log(semua[0][4]);
+console.log(semua[1][0]);
+console.log(semua[1][2]);
+console.log(semua[1][4]);
+console.log(semua[2][0]);
+console.log(semua[2][2]);
+console.log(semua[2][4]);
